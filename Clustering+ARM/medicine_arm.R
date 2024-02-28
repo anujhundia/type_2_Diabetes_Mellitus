46321bbe66a6b7b6529e24fe49f00c0f802e6ead
anujhundia <51014053+anujhundia@users.noreply.github.com>
2024-02-27 22:23:11 -0700

    Add files via upload

---
+++ Clustering+ARM/medicine_arm.R
@@ -0,0 +1,27 @@
+library(viridis)
+library(arules)
+library(TSP)
+library(data.table)
+library(tcltk)
+library(dplyr)
+library(devtools)
+library(purrr)
+library(tidyr)
+library(arulesViz)
+setwd("D:/CU/Machine_Learning/Project/ARM")
+medicine_data <- read.transactions("medicine_arm.csv",
+                                   rm.duplicates = FALSE, 
+                                   format = "basket",  ##if you use "single" also use cols=c(1,2)
+                                   sep=",",  ## csv file
+                                   cols=1) ## The dataset HAS row numbers
+#inspect(medicine_data)
+FrulesK = arules::apriori(medicine_data, parameter = list(support=.5, 
+                                                          confidence=.5, minlen=2))
+#inspect(FrulesK)
+itemFrequencyPlot(medicine_data, topN=20, type="absolute")
+
+SortedRulesK <- sort(FrulesK, by="confidence", decreasing=TRUE)
+inspect(SortedRulesK[1:10])
+
+(summary(SortedRulesK))
+plot(SortedRulesK, method="graph", engine="interactive")
